80ec0d07f88a4fd59b9e35d9fed2ee419dc19487
Georg Ringer <g.ringer@fonda.at>
2026-04-09 09:23:09 +0200

    [FEATURE] Add more meta data

--- Classes/Service/BugsnagService.php
+++ Classes/Service/BugsnagService.php
@@ -4,6 +4,13 @@ namespace GeorgRinger\Bugsnag\Service;
 
 use Bugsnag\Client;
 use Bugsnag\Handler;
+use Bugsnag\Report;
+use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Http\ApplicationType;
+use TYPO3\CMS\Core\Routing\PageArguments;
+use TYPO3\CMS\Core\Site\Entity\Site;
+use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 
 /**
  * Sends Exception to Bugsnag
@@ -12,8 +19,6 @@ class BugsnagService
 {
     /**
      * Sends exception to Bugsnag
-     *
-     * @param \Exception $exception
      */
     public function sendException(\Throwable $exception): void
     {
@@ -21,22 +26,75 @@ class BugsnagService
         $bugsnagApiKey = $extensionConfiguration['apiKey'] ?: (getenv('BUGSNAG_API_KEY') ?: '');
 
         if ($bugsnagApiKey !== '') {
-
             $bugsnag = Client::make($bugsnagApiKey);
 
-            // Set app type
-            if (PHP_SAPI === 'cli') {
-                $bugsnag->setAppType('Cli');
-            } else {
-                $bugsnag->setAppType('Web');
-            }
+            $request = $GLOBALS['TYPO3_REQUEST'] ?? null;
 
-            // Set context
-            $context = \TYPO3\CMS\Core\Core\Environment::getContext();
-            $bugsnag->setReleaseStage((string)$context);
+            if (PHP_SAPI === 'cli') {
+                $appType = 'CLI';
+            } elseif ($request !== null && ApplicationType::fromRequest($request)->isBackend()) {
+                $appType = 'BE';
+            } else {
+                $appType = 'FE';
+            }
+            $bugsnag->setAppType($appType);
+            $bugsnag->setReleaseStage((string)Environment::getContext());
 
             Handler::register($bugsnag);
-            $bugsnag->notifyException($exception);
+            $bugsnag->notifyException($exception, function (Report $report) use ($appType, $request): void {
+                $metadata = [
+                    'typo3' => [
+                        'version' => VersionNumberUtility::getNumericTypo3Version(),
+                        'context' => (string)Environment::getContext(),
+                    ],
+                    'request' => [
+                        'url' => $_SERVER['REQUEST_URI'] ?? null,
+                        'method' => $_SERVER['REQUEST_METHOD'] ?? null,
+                        'referer' => $_SERVER['HTTP_REFERER'] ?? null,
+                    ],
+                    'server' => [
+                        'memory_peak_mb' => round(memory_get_peak_usage(true) / 1024 / 1024, 2),
+                        'php_version' => PHP_VERSION,
+                        'composer_mode' => Environment::isComposerMode(),
+                    ],
+                ];
+
+                if ($request !== null && $appType === 'BE') {
+                    $beUser = $GLOBALS['BE_USER'] ?? null;
+                    if ($beUser !== null && isset($beUser->user['uid'])) {
+                        $metadata['typo3']['be_user'] = [
+                            'uid' => $beUser->user['uid'],
+                            'username' => $beUser->user['username'],
+                        ];
+                    }
+                } elseif ($request !== null && $appType === 'FE') {
+                    $site = $request->getAttribute('site');
+                    $routing = $request->getAttribute('routing');
+                    if ($site instanceof Site) {
+                        $metadata['typo3']['site'] = $site->getIdentifier();
+                    }
+                    if ($routing instanceof PageArguments) {
+                        $metadata['typo3']['PageId'] = $routing->getPageType() === '0' ? $routing->getPageId() : (sprintf('%s [%s]', $routing->getPageId(), $routing->getPageType()));
+
+                        $routingInfo = array_filter([
+                            'arguments' => $routing->getArguments(),
+                            'staticArguments' => $routing->getStaticArguments(),
+                            'dynamicArguments' => $routing->getDynamicArguments(),
+                            'queryArguments' => $routing->getQueryArguments(),
+                            'routeArguments' => $routing->getRouteArguments(),
+                        ]);
+                        if (!empty($routingInfo)) {
+                            $metadata['routing'] = $routingInfo;
+                        }
+                    }
+                    $language = $request->getAttribute('language');
+                    if ($language instanceof SiteLanguage) {
+                        $metadata['typo3']['language'] = sprintf('%s (%s)', (string)$language->getLocale(), $language->getLanguageId());
+                    }
+                }
+
+                $report->setMetaData($metadata);
+            });
             $bugsnag->flush();
         }
     }
